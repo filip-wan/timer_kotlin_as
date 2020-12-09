9d2a70852ceb6d7b447bad0a9c25ea9f39ef1fc1
Filip Wanat <filip.k.wanat@gmail.com>
2020-12-09 10:56:38 +0100

    Remove comments

--- app/src/main/java/com/example/timer_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/timer_kotlin/MainActivity.kt
@@ -21,11 +21,5 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
-//        val start: Button = findViewById(R.id.button)
-//
-//        start.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                    .setAction("Action", null).show()
-//        }
     }
 }
